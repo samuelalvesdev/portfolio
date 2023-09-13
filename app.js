5bf36db9448e831223c267f6d7abe913ca047184
Samuel Alves <samuelalves@utic.pe.senac.br>
2023-09-13 11:27:37 -0300

    finalização da nav-link scroll responsive

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const images = document.querySelectorAll(".images img");
 const prev_btn = document.querySelector(".prev-btn");
 const next_btn = document.querySelector(".next-btn");
 
+const links = document.querySelectorAll(".nav-link");
+
 window.addEventListener("scroll", () => {
+    activeLink();
     if(!skillsPlayed) skillsCounter();
     if (!mlPlayed) mlCounter();
 });
@@ -170,3 +173,23 @@ const swiper = new Swiper(".swiper", {
       clickable: true,
     },
   });
+
+    /*---------- Change Active Link On Scroll ----------*/
+
+function activeLink(){
+    let sections = document.querySelectorAll("section[id]")
+    let passedSections = Array.from(sections).map((sct, i) => {
+        return { 
+            y: sct.getBoundingClientRect().top - header.offsetHeight,
+            id: i,
+        };
+    })
+    .filter((sct) => sct.y <= 0);
+    
+    let currSectionID = passedSections.at(-1).id;
+
+    links.forEach(l => l.classList.remove("active"));
+    links[currSectionID].classList.add("active");
+}
+
+activeLink();
